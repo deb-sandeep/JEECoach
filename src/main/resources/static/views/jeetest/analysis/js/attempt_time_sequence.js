22ff3127f1a048430d129c90a3be65082d53b3db
Sandeep Deb <deb.sandeep@gmail.com>
2022-01-17 17:43:13 +0530

    Baseline

--- src/main/resources/static/views/jeetest/analysis/js/attempt_time_sequence.js
+++ src/main/resources/static/views/jeetest/analysis/js/attempt_time_sequence.js
@@ -3,19 +3,19 @@ var questionAttemptMap = {} ;
 function Activity( qNum ) {
 	this.questionNum = qNum ;
 	this.start = 0 ;
-	this.duration = 0 ;
+	this.duration = 5 ;
 	this.transitionEvent = null ;
 }
 
 function GanttActivity( activity ) {
     
-    var attempt = questionAttemptMap[activity.questionNum] ;
-    var isAnsCorrect = attempt.isCorrect ? "" : "[X] " ;
+    var attempt  = questionAttemptMap[activity.questionNum] ;
     
-	this.label = isAnsCorrect + activity.questionNum ;
-	this.start = activity.start ;
+	this.label    = activity.questionNum ;
+	this.start    = activity.start ;
 	this.duration = activity.duration ;
-	this.color = getColor() ;
+	this.color    = getColor() ;
+	this.tooltip  = getTooltip() ;
 	
 	function getColor() {
 		
@@ -23,6 +23,10 @@ function GanttActivity( activity ) {
 		if( type == null ) {
 			return "#B9D7FB" ;
 		}
+        else if( type == "LEAD_BLOCK" ) {
+            if( attempt.isCorrect ) return "#00FF00" ;
+            else return "#FF0000" ;
+        }
 		else if( type == ClickStreamEvent.prototype.ANSWER_SAVE ) {
 			return "#00FF00" ;
 		}
@@ -37,18 +41,37 @@ function GanttActivity( activity ) {
 		}
 		return "#000000" ;
 	}
+	
+    function getTooltip() {
+        
+        var tooltip = activity.questionNum ;
+        tooltip += "<br>" + activity.duration + " sec" ;
+        if( activity.transitionEvent != null ) {
+            tooltip += "<br>" + activity.transitionEvent ;
+        }
+        if( !attempt.isCorrect ) {
+            tooltip += "<br> Incorrect answer" ;
+            tooltip += "<br> Root cause - " + attempt.rootCause ;
+        }
+        else {
+            tooltip += "<br> Correct answer" ;
+        }
+        tooltip += "<br> Total time = " + attempt.timeSpent ;
+        return tooltip ;
+    }
 }
 
 sConsoleApp.controller( 'TestAttemptTimeSequenceController', function( $scope, $http, $location, $routeParams ) {
     
-    var questionIdNumMap = {} ;
+    var questionIdNumMap        = {} ;
+    var questionNumIndexMap     = {} ;
     var questionActivityListMap = {} ;
+    
+    var allActivities = [] ;
     var lapMarkers = [] ;
     
-    var graph = null ;
-    var allGraphData = [] ;
-    var currentGraphData = [] ;
-    var graphTooltips = [] ;
+    var graphData = [] ;
+    var tooltips  = [] ;
 
 	$scope.$parent.navBarTitle = "Test Attempt Time Sequence" ;
 	$scope.testAttemptId = $routeParams.id ;
@@ -134,8 +157,11 @@ sConsoleApp.controller( 'TestAttemptTimeSequenceController', function( $scope, $
     function createQuestionMaps() {
         
         lapMarkers.length = 0 ;
-        questionIdNumMap = {} ;
-        questionAttemptMap = {} ;
+        
+        questionIdNumMap        = {} ;
+        questionAttemptMap      = {} ;
+        questionNumIndexMap     = {} ;
+        questionActivityListMap = {} ;
         
     	for( var i=0; i<$scope.questions.length; i++ ) {
     		var question = $scope.questions[i] ;
@@ -144,7 +170,8 @@ sConsoleApp.controller( 'TestAttemptTimeSequenceController', function( $scope, $
     		var qNum = getQNum( i ) ;
     		
     		questionIdNumMap[ question.id ] = qNum ;
-    		questionAttemptMap[ qNum ] = attempt ;
+    		questionNumIndexMap[ qNum ]     = i ;
+    		questionAttemptMap[ qNum ]      = attempt ;
     		questionActivityListMap[ qNum ] = [] ;
     	}
     }
@@ -155,6 +182,8 @@ sConsoleApp.controller( 'TestAttemptTimeSequenceController', function( $scope, $
     	var currentLapStartTime = 0 ;
     	var currentLapEndTime = 0 ;
     	var currentLapIndex = -1 ;
+    	
+    	allActivities.length = 0 ;
     	
     	for( var i=0; i<clickEvents.length; i++ ) {
     		
@@ -169,6 +198,7 @@ sConsoleApp.controller( 'TestAttemptTimeSequenceController', function( $scope, $
     			activity.start = Math.floor( event.timeMarker/1000 ) ;
     			
     			questionActivityListMap[ qNum ].push( activity ) ;
+    			allActivities.push( activity ) ;
     			
     			if( lastActivity != null ) {
     				lastActivity.duration = activity.start - lastActivity.start ;
@@ -206,52 +236,29 @@ sConsoleApp.controller( 'TestAttemptTimeSequenceController', function( $scope, $
     
     function setUpActivityGraph() {
         
-        graphTooltips.length = 0 ;
-        allGraphData.length = 0 ;
-        currentGraphData.length = 0 ;
+        tooltips.length = 0 ;
+        graphData.length = 0 ;
         
+        initializeGraphData() ;
         setUpRGraph() ;
+        animateGantt() ;
+    }
+    
+    function initializeGraphData() {
         
         for( var i=0; i<$scope.questions.length; i++ ) {
-            var attempt = $scope.questionAttempts[i] ;
+            
             var qNum = getQNum( i ) ;
-            
-            var activities = questionActivityListMap[ qNum ] ;
             var ganttActivities = [] ;
+            var activity = new Activity( qNum ) ;
             
-            // If this question has not been visited, no click stream activity
-            // would have been generated. This would cause RGraph to throw
-            // and error. We inject a dummy activity of duration 0.
-            if( activities.length == 0 ) {
-                var activity = new Activity( qNum ) ;
-                activities.push( activity ) ;
-            }
+            activity.transitionEvent = "LEAD_BLOCK" ;
             
-            for( var j=0; j<activities.length; j++ ) {
-                var activity = activities[j] ;
-                ganttActivities.push( new GanttActivity( activity ) ) ;
-                
-                var tooltip = qNum ;
-                tooltip += "<br>" + activity.duration + " sec" ;
-                if( activity.transitionEvent != null ) {
-                    tooltip += "<br>" + activity.transitionEvent ;
-                }
-                if( !attempt.isCorrect ) {
-                    tooltip += "<br> Incorrect answer" ;
-                    tooltip += "<br> Root cause - " + attempt.rootCause ;
-                }
-                else {
-                    tooltip += "<br> Correct answer" ;
-                }
-                tooltip += "<br> Total time = " + attempt.timeSpent ;
-                
-                graphTooltips.push( tooltip ) ;
-            }
+            var ganttActivity = new GanttActivity( activity ) ;
             
-            allGraphData.push( ganttActivities ) ;
+            ganttActivities.push( ganttActivity ) ;
+            graphData.push( ganttActivities ) ;
         }
-        
-        animateGantt() ;
     }
     
     function setUpRGraph() {
@@ -271,17 +278,16 @@ sConsoleApp.controller( 'TestAttemptTimeSequenceController', function( $scope, $
         }
 
         RGraph.reset( canvas ) ;
-        
-        graph = new RGraph.Gantt({
+        new RGraph.Gantt({
             id: 'testAttemptGantt',
-            data: currentGraphData,
+            data: graphData,
             options: {
                 backgroundGrid : true,
                 backgroundGridVlinesCount: xAxisDivs,
                 xaxisLabels : xLabels,
                 xaxisScaleMax: maxTime,
                 yaxisLabelsSize: 10,
-                tooltips: graphTooltips,
+                tooltips: tooltips,
                 tooltipsEvent : 'mousemove', 
                 hmargin: 5,
                 colorsDefault: 'green',
@@ -293,13 +299,33 @@ sConsoleApp.controller( 'TestAttemptTimeSequenceController', function( $scope, $
     
     function animateGantt() {
         
-        if( allGraphData.length > 0 ) {
-            currentGraphData.push( allGraphData.shift() ) ;
-            RGraph.redraw() ;
-            
-            setTimeout( animateGantt, 1000 ) ;
-        }        
-    }    
+        if( allActivities.length == 0 ) return ;
+        
+        var activity = allActivities.shift() ;
+        var qIndex   = questionNumIndexMap[ activity.questionNum ] ;
+        
+        var ganttActivity = new GanttActivity( activity ) ;
+        
+        graphData[qIndex].push( ganttActivity ) ;
+        recreateTooltips() ;
+        
+        RGraph.redraw() ;
+        
+        if( $scope.autoRefresh ) {
+            animateGantt() ;
+        }
+        else {
+            setTimeout( animateGantt, 200 ) ;
+        }
+    }
     
+    function recreateTooltips() {
+        tooltips.length = 0 ;
+        for( var i=0; i<graphData.length; i++ ) {
+            for( var j=0; j<graphData[i].length; j++ ) {
+                tooltips.push( graphData[i][j].tooltip ) ;
+            }
+        }
+    }
     // --- [END] Local functions
 } ) ;
